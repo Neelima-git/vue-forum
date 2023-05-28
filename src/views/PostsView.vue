bc56ee630f8ae157e003be396459b9f2666cb0c9
nbellana <bellana.neelima2@gmail.com>
2023-05-28 13:35:50 +0530

    .

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -4,16 +4,16 @@
       <nav class="navbar bg-secondary bg-opacity-50">
         <div class="container-fluid">
           <a class="navbar-brand">Forum Posts</a>
-          <form class="d-flex" role="search">
+          <div class="d-flex" >
             <input
-              class="form-control me-2"
+              class="form-control me-3"
               type="search"
               placeholder="Search"
               aria-label="Search"
               v-model.trim="searchQuery"
             />
             <button class="btn btn-outline-danger text-red">Delete</button>
-          </form>
+          </div>
         </div>
       </nav>
   
